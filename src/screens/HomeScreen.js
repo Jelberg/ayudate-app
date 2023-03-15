e716686f7ae611009473a33cce4b0bbcb06901e9
Jessica Elberg <elbergjessica@gmail.com>
2023-03-15 01:58:06 -0400

    delete button from home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,17 +4,12 @@ import React from "react";
 export default function HomeScreen(props) {
   const { navigation } = props;
 
-  const goToPage = () => {
-    navigation.navigate("Settings");
-  };
-
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
       <Text>HomeScreen</Text>
       <Text>HomeScreen</Text>
       <Text>HomeScreen</Text>
-      <Button onPress={goToPage} title="Go to Page" />
     </SafeAreaView>
   );
 }
